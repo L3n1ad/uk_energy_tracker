f1a2eb4fdcccef34857780dec721886b70973396
L3n1ad <danieltoth0910@gmail.com>
2019-11-28 11:53:22 +0000

    imported google chart to main js"

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGoogleCharts from 'vue-google-charts'
+
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleCharts)
 
 new Vue({
   render: h => h(App),
